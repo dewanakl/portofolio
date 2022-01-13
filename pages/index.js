951e2bc17fa76a45540883742e90851546ce6b1b
dewanakl <53048107+dewanakl@users.noreply.github.com>
2022-01-13 18:13:46 +0700

    cobaaaaa

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ data }) {
+
+  const recipes = data.recipes;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +18,10 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Link href="/about">
+          <a>{recipes[0].title}</a>
+        </Link>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -66,4 +73,14 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      data: {
+        recipes: [{ title: "coba ya wkwk" }],
+      },
+    },
+  }
 }
