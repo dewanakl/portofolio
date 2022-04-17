2893becc87de9af9ea0e7185ae7aa295f7c76ad1
dewanakl <53048107+dewanakl@users.noreply.github.com>
2022-04-17 17:32:58 +0700

    coba yach :+1:

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,25 @@ import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to My Blog</h1>
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <Link href="/posts/first-post">
-              <h3>First Post</h3>
-            </Link>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry...
-            </p>
-          </div>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Next App</title>
+            </Head>
+            <main className={styles.main}>
+                <h1 className={styles.title}>Welcome to My Blog</h1>
+                <Link href="/posts/first-post">
+                    <div className={styles.grid}>
+                        <div className={styles.card}>
+                            <h3>First Post</h3>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+                            </p>
+                        </div>
+                    </div>
+                </Link>
+            </main>
+            <footer className={styles.footer}></footer>
         </div>
-      </main>
-      <footer className={styles.footer}>
-      </footer>
-    </div>
-  );
+    );
 }
