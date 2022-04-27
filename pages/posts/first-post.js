1fa254c93a751954f7e2bad3edd439c745e514a1
dewanakl <53048107+dewanakl@users.noreply.github.com>
2022-04-27 10:01:37 +0700

    add back

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,10 @@
 import Layout from "../../components/layout";
+import Link from "next/link";
 
 export default function FirstPost() {
     return (
         <Layout pageTitle="First">
+            <Link href="/">Back</Link>
             <h1>First Post</h1>
         </Layout>
     );
--- pages/posts/second-post.js
+++ pages/posts/second-post.js
@@ -1,8 +1,10 @@
 import Layout from "../../components/layout";
+import Link from "next/link";
 
 export default function SecondPost() {
     return (
         <Layout pageTitle="Second">
+            <Link href="/">Back</Link>
             <h1>Second Post</h1>
             <p>
                 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old
