9d707c6ff801627936a95f5647a72106c3fdf52b
dewanakl <53048107+dewanakl@users.noreply.github.com>
2022-04-28 11:45:27 +0700

    hehe

--- components/layout/index.js
+++ components/layout/index.js
@@ -3,12 +3,12 @@ import Head from "next/head";
 function Layout(props) {
     const { children, pageTitle } = props;
     return (
-        <div>
+        <>
             <Head>
                 <title>{pageTitle}</title>
             </Head>
             {children}
-        </div>
+        </>
     );
 }
 
